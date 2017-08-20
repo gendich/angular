df4360460a7e9f89123448cb69ec00be702da635
BOBROV <bobrov_ai_13@mf.grsu.by>
2017-08-20 16:02:26 +0300

    Add ergast factory to the project

--- app/championship/championship.js
+++ app/championship/championship.js
@@ -9,29 +9,26 @@ angular.module('myApp.championship', ['ngRoute'])
         });
     }])
 
-    .controller('ChampionshipCtrl', ['$scope', function($scope) {
-        $scope.driversList = [
-            {
-                Driver: {
-                    givenName: 'Sebastian',
-                    familyName: 'Vettel'
-                },
-                points: 322,
-                nationality: "German",
-                Constructors: [
-                    {name: "Red Bull"}
-                ]
-            },
-            {
-                Driver: {
-                    givenName: 'Fernando',
-                    familyName: 'Alonso'
-                },
-                points: 207,
-                nationality: "Spanish",
-                Constructors: [
-                    {name: "Ferrari"}
-                ]
-            }
-        ];
+    .factory('ergastAPIservice', function($http) {
+
+        var ergastAPI = {};
+
+        ergastAPI.getDrivers = function() {
+            return $http({
+                method: 'JSONP',
+                url: 'http://ergast.com/api/f1/2013/driverStandings.json?callback=JSON_CALLBACK'
+            });
+        };
+
+        return ergastAPI;
+    })
+
+    .controller('ChampionshipCtrl', ['$scope', '$ergastAPIservice', function($scope, ergastAPIservice) {
+        $scope.nameFilter = null;
+        $scope.driversList = [];
+
+        ergastAPIservice.getDrivers().success(function (response) {
+            //Dig into the responde to get the relevant data
+            $scope.driversList = response.MRData.StandingsTable.StandingsLists[0].DriverStandings;
+        });
     }]);
